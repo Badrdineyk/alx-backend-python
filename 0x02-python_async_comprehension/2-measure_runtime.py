ee0387a1e40aeffae43f3a3f985cb495bee02cec
Badrdineyk <badrdineyk9@gmail.com>
2024-10-15 12:15:30 -0700

    Tasks are solved

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+"""
+Async Comprehensions
+A script to measure the runtime for four parallel async comprehensions.
+"""
+
+from asyncio import gather
+from time import time
+
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """ Measures the runtime for four parallel async comprehensions. """
+    start = time()
+    tasks = [async_comprehension() for i in range(4)]
+    await gather(*tasks)
+    end = time()
+    return (end - start)
